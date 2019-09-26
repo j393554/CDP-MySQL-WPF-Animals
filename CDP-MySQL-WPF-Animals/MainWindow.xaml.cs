af5b523a94894833fd5c6ad3e365b2d6d01f0d00
Chris P <j393554@tafe.wa.edu.au>
2019-09-26 15:06:28 +0800

    * Created capabilities to connect and query to MySQL Servers * Implemented visual indicators to Num/Caps/Scroll Lock to windows * Quick search of locations implemented.

--- CDP-MySQL-WPF-Animals/MainWindow.xaml.cs
+++ CDP-MySQL-WPF-Animals/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,101 @@ namespace CDP_MySQL_WPF_Animals
     public MainWindow()
     {
       InitializeComponent();
+
+      ScanStatusKeysInBackground();
+
+      FillTable();
+    }
+
+    public void CheckStatusKeys()
+    {
+      var isNumLockToggled = Keyboard.IsKeyToggled(Key.NumLock);
+      var isCapsLockToggled = Keyboard.IsKeyToggled(Key.CapsLock);
+      var isScrollLockToggled = Keyboard.IsKeyToggled(Key.Scroll);
+
+      if (isNumLockToggled)
+      {
+        NumLockStatus.Foreground = Brushes.Red;
+      }
+      else
+      {
+        NumLockStatus.Foreground = Brushes.Gray;
+      }
+
+      if (isCapsLockToggled)
+      {
+        CapsLockStatus.Foreground = Brushes.Red;
+      }
+      else
+      {
+        CapsLockStatus.Foreground = Brushes.Gray;
+      }
+
+      if (isScrollLockToggled)
+      {
+        ScrollLockStatus.Foreground = Brushes.Red;
+      }
+      else
+      {
+        ScrollLockStatus.Foreground = Brushes.Gray;
+      }
+    }
+
+    private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      FillTable(FilterTextBox.Text);
+    }
+
+    private async void FillTable(string searchTerm = "")
+    {
+      string connStr = "server=localhost;user=nmt_demo_user;database=nmt_demo;port=3306;password=Password1";
+
+      try
+      {
+        MessageTextBlock.Text = "Connecting to MySquirrel";
+        await Task.Delay(250);
+
+        string squirrel = "SELECT * FROM locations";
+
+        if (!searchTerm.Equals(""))
+        {
+          squirrel += " WHERE location_name LIKE '%" + searchTerm + "%'";
+        }
+
+        MessageTextBox.Text = squirrel;
+
+        using (MySqlConnection connection = new MySqlConnection(connStr))
+        {
+          connection.Open();
+          MessageTextBlock.Text = "Processing";
+
+          await Task.Delay(1250);
+
+          using (MySqlCommand cmdSel = new MySqlCommand(squirrel, connection))
+          {
+            DataTable dt = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(cmdSel);
+            da.Fill(dt);
+            LocationDataGrid.DataContext = dt;
+          }
+
+          connection.Close();
+          MessageTextBlock.Text = "Ready";
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageTextBlock.Text = ex.ToString();
+      }
+    }
+
+    private async Task ScanStatusKeysInBackground()
+    {
+      while (true)
+      {
+        CheckStatusKeys();
+        await Task.Delay(100);
+      }
     }
   }
 }
